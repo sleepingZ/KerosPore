ed562ca5ef55053a24e1426185e8aaf3d96986b1
sleepingZ <sleepingz2008@qq.com>
2016-12-06 20:15:48 +0800

    Functions for datafile modifying reorgnized. New methods for bond and angle data modifying added.

--- tools/datafile_mod.py
+++ tools/datafile_mod.py
@@ -31,7 +31,6 @@ def explore(filename,Key1,Key2):
     f.close()
     return start,end
 
-			        
 def writeCharge(g,line,insertflag):
     if (insertflag):
         line=line.split()
@@ -43,32 +42,105 @@ def writeCharge(g,line,insertflag):
         g.write(line)
     else:
         g.write(line)
-  
-def addAtomType(filename,type_num_old,type_num_new):
-    mass_start,mass_end = explore(filename,'Masses','Bond Coeffs')
-    f=open(filename,'r')
-    lines =f.readlines()
-    f.close()
-    for line in lines:
-        if line.split()[-2:] == ['atom','types']:
-            index = lines.index(line)
-            lines[index] = ' '.join([str(type_num_new),'atom','types'])+'\n'
-            break
-    masslines = lines[mass_start:mass_end-1]
-    massnow = type_num_old + 1
-    for j in range(len(masslines)):
-        line = masslines[j]
-        k = j+mass_start
-        if len(line.split())>1:
-#            if (line.split()[0] == str(type_num_old)) and (line.split()[0] <str(type_num_new)):
-#                lines.insert(linenow,'%d 1.0\n'%massnow)
-#                massnow += 1
-            if (line.split()[0] == str(type_num_old)):
-                insert_start_pos = k+1
-    for i in range(type_num_new-type_num_old):
-        lines.insert(insert_start_pos+i,'%d 1.0\n'%(massnow+i))
-    f.close()
-    g=open('data.dreiding','w')
-    g.writelines(lines)
-    g.close()
+
+class dataFile:
+    def __init__(self,inputfile,outputfile = "data.dreiding"):
+        f = open(inputfile,'r')
+        lines = f.readlines()
+        f.close()
+        self.lines = lines
+        self.inputfile = inputfile
+        self.outputfile = outputfile
+        self.basicInfo()
     
+    def basicInfo(self):
+        lines = self.lines
+        for line in lines:
+            line_seq = line.split()
+            if line_seq[-2:] == ['atom','types']:
+                self.nAtomType = int(line_seq[0])
+            if line_seq[-2:] == ['bond','types']:
+                self.nBondType = int(line_seq[0])
+            if line_seq[-2:] == ['angle','types']:
+                self.nAngleType = int(line_seq[0])
+
+    def explore(self,Key1,Key2):
+        lines = self.lines
+        for line in lines:
+            if line.startswith(Key1):
+                start = lines.index(line) + 1
+            if line.startswith(Key2):
+                end = lines.index(line) + 1
+        return start,end
+
+    def addAtomType(self,type_num_old,type_num_new):
+        mass_start,mass_end = self.explore('Masses','Bond Coeffs')
+        lines = self.lines
+        for line in lines:
+            if line.split()[-2:] == ['atom','types']:
+                index = lines.index(line)
+                lines[index] = ' '.join([str(type_num_new),'atom','types'])+'\n'
+                break
+        masslines = lines[mass_start:mass_end-1]
+        massnow = type_num_old + 1
+        for j in range(len(masslines)):
+            line = masslines[j]
+            k = j+mass_start
+            if len(line.split())>1:
+                if (line.split()[0] == str(type_num_old)):
+                    insert_start_pos = k+1
+        for i in range(type_num_new-type_num_old):
+            lines.insert(insert_start_pos+i,'%d 1.0\n'%(massnow+i))
+    
+    def addBondType(self,type_num_old,type_num_new):
+        bond_start,bond_end = self.explore('Bond Coeffs','Angle Coeffs')
+        lines = self.lines
+        for line in lines:
+            if line.split()[-2:] == ['bond','types']:
+                index = lines.index(line)
+                lines[index] = ' '.join([str(type_num_new),'bond','types'])+'\n'
+                break
+        bondlines = lines[bond_start:bond_end-1]
+        bondnow = type_num_old + 1
+        for j in range(len(bondlines)):
+            line = bondlines[j]
+            k = j+bond_start
+            if len(line.split())>1:
+                if (line.split()[0] == str(type_num_old)):
+                    insert_start_pos = k+1
+        for i in range(type_num_new-type_num_old):
+            lines.insert(insert_start_pos+i,'%d 1.0 1.0\n'%(bondnow+i))
+
+    def addAngleType(self,type_num_old,type_num_new):
+        angle_start,angle_end = self.explore('Angle Coeffs','Dihedral Coeffs')
+        lines = self.lines
+        for line in lines:
+            if line.split()[-2:] == ['angle','types']:
+                index = lines.index(line)
+                lines[index] = ' '.join([str(type_num_new),'angle','types'])+'\n'
+                break
+        anglelines = lines[angle_start:angle_end-1]
+        anglenow = type_num_old + 1
+        for j in range(len(anglelines)):
+            line = anglelines[j]
+            k = j+angle_start
+            if len(line.split())>1:
+                if (line.split()[0] == str(type_num_old)):
+                    insert_start_pos = k+1
+        for i in range(type_num_new-type_num_old):
+            lines.insert(insert_start_pos+i,'%d 1.0 1.0\n'%(anglenow+i))
+    
+    def addExtra(self):
+        lines = self.lines
+        for line in lines:
+            if line.strip().split()[-2:] == ['improper','types']:
+                index = lines.index(line) + 1
+                break
+        lines.insert(index,'4 extra special per atom\n')
+        lines.insert(index,'6 extra angle per atom\n')
+        lines.insert(index,'4 extra bond per atom\n')
+            
+    def output(self):
+        g=open(self.outputfile,'w')
+        g.writelines(self.lines)
+        g.close()
